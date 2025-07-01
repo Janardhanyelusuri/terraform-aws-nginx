f53e8e51f501de88d5ce7003f56d390a2c0a0649
Janardhan Yelusuri <123528298+Janardhanyelusuri@users.noreply.github.com>
2025-07-01 17:16:01 +0530

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -18,6 +18,20 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "NAT-Gateway"
+  }
+}
+
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 }
@@ -28,9 +42,27 @@ resource "aws_route" "internet_access" {
   gateway_id             = aws_internet_gateway.igw.id
 }
 
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
 }
 
 resource "aws_security_group" "web_sg" {
